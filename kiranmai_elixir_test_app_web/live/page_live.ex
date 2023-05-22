0236cb68c8df0d45c997760b43dd84df6e56d6ca
kiranmai-muthoju <83878314+kiranmai-muthoju@users.noreply.github.com>
2023-05-22 12:07:52 -0700

    Added simple item add

--- kiranmai_elixir_test_app_web/live/page_live.ex
+++ kiranmai_elixir_test_app_web/live/page_live.ex
@@ -1,1 +1,27 @@
-init
+defmodule KiranmaiElixirTestApp.Web.Page do
+  use Phoenix.LiveView
+
+  def render(assigns) do
+    ~H"""
+    <div class="container">
+      <h1>Welcome to Kiranmai Elixir Test App!</h1>
+      <p>Current time: <%= @current_time %></p>
+      <ul>
+        <%= for item <- @items do %>
+          <li><%= item %></li>
+        <% end %>
+      </ul>
+      <button phx-click="add_item">Add Item</button>
+    </div>
+    """
+  end
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, current_time: DateTime.utc_now(), items: ["Item 1", "Item 2"])}
+  end
+
+  def handle_event("add_item", _, socket) do
+    updated_items = ["New Item" | socket.assigns.items]
+    {:noreply, assign(socket, items: updated_items)}
+  end
+end
